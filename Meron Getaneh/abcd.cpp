a71825815f5557ac6e0e99ddb8da4ed0d4ff071a
Meron Getaneh <124381940+meronheron@users.noreply.github.com>
2023-03-16 18:06:04 +0300

    Add files via upload

---
+++ Meron Getaneh/abcd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+    cout<<"enter the number of rows\n";
+    cin>>a;
+    for(int k=1;k<=a;k++){
+    for(char n=65;n<=69;n++){
+    cout<<n<<" ";
+    }
+    cout<<endl;
+    }
+}
