e372b62d43d431493ac5bab67623d7c8cf9def47
Meron Getaneh <124381940+meronheron@users.noreply.github.com>
2023-03-16 18:19:22 +0300

    Add files via upload

---
+++ Meron Getaneh/number.cpp
@@ -0,0 +1,17 @@
+//number pattern
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the number of rows\n";
+    cin>>a;
+    cout<<"enter the number of columns\n";
+    cin>>b;
+    for(int k=1;k<=a;k++){
+    for(int n=1;n<=b;n++){
+    cout<<n;
+    }
+    cout<<endl;
+    }
+}
