9f5fd494c428af06ae8cbbb6749ae270e613469a
Meron Getaneh <124381940+meronheron@users.noreply.github.com>
2023-03-16 18:09:10 +0300

    Add files via upload

---
+++ Meron Getaneh/half_pyramid.cpp
@@ -0,0 +1,17 @@
+
+//half pyramid
+#include<iostream>
+using namespace std;
+int main()
+{
+for(int i=1; i<=5; i++)
+      {
+  for(int j=1; j<=i; j++)
+    {
+      cout<<"* ";
+    }
+      cout<<endl;
+  }
+}
+
+
