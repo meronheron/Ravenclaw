0b90ab1182d781088db393644c6325be7a26f084
Meron Getaneh <124381940+meronheron@users.noreply.github.com>
2023-03-16 18:14:29 +0300

    Add files via upload

---
+++ Meron Getaneh/inverted_half_pyramid.cpp
@@ -0,0 +1,19 @@
+//inverted half pyramid
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  for(int j=5; j>0; j--)
+  {
+      for(int i=0; i<j; i++)
+      {
+          cout<<"*";
+      }
+    cout<<endl;
+  }
+
+return 0;
+}
+
+
