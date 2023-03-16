3c67456bc8f52c01f2b2fbeda1c92a07f5866057
Meron Getaneh <124381940+meronheron@users.noreply.github.com>
2023-03-16 19:46:16 +0300

    Add files via upload

---
+++ Meron Getaneh/alphabet_half_pyramid.cpp
@@ -0,0 +1,14 @@
+//alphabet half pyramid
+#include<iostream>
+using namespace std;
+int main() {
+ int a;
+ char b;
+ for(a=1;a<=5;a++){
+    for(b=65;b<=64+a;b++){
+        cout<<b<<"";
+    }
+    cout<<endl;
+ }
+
+}
