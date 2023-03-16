451292c8c37bd7f6553555c644e6e580019dfe16
Meron Getaneh <124381940+meronheron@users.noreply.github.com>
2023-03-16 03:27:13 +0300

    Add files via upload

---
+++ Meron Getaneh/bmi3.cpp
@@ -0,0 +1,31 @@
+// calculate bmi
+#include <iostream>
+using namespace std;
+int main()
+{
+    char gender;
+    float weight;
+    float height;
+    float bmi;
+    cout<<"enter gender:\n";
+    cin>>gender;
+    cout<<"enter your weight:\n";
+    cin>>weight;
+    cout<<"enter your height:\n";
+    cin>>height;
+    bmi=weight/(height*height);
+if (gender=='f'&& bmi<18){
+  cout<<"you have to eat:\n";
+}else if(gender=='f'&& bmi>23){
+ cout<<"start working out:\n";
+}else if(gender=='f'&& bmi>=18&& bmi<=23){
+cout<<"you are doing great:\n";
+}else if(gender=='m'&& weight<20){
+    cout<<"you are overweight:\n";
+}else if(gender=='m'&& weight>30){
+cout<<"hit the gym :\n";
+}else{
+    cout<<"your doing well:\n";
+}
+}
+
